098a34dbb4c7f625c7c898b8d6a4611d56243e1d
Maciej Ziemichód <maciej.ziemichod@gmail.com>
2020-07-25 12:55:48 +0200

    example migration

--- deployment/build.php
+++ deployment/build.php
@@ -1,7 +1,7 @@
 <?php
 
+include "deployment/migrate.php";
+
 include "events/topmenu_change.php";
 
 include "deployment/scan_routes.php";
-
-?>
--- deployment/migrate.php
+++ deployment/migrate.php
@@ -1,10 +1,16 @@
 <?php
 
 // var_dump(fetchArray("SELECT * FROM products WHERE product_id = ?", [7]));
-if (!tableExists(clean("asdadasafds"))) {
-    query("CREATE TABLE `asdadasafds` (
+if (!tableExists("asdadasafds")) {
+  query("CREATE TABLE asdadasafds (
         `dasfdsf` int(11) NOT NULL
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;");
 
-    echo "table asdadasafds created!";
+  echo "table asdadasafds created! <br>";
+}
+
+if (!columnExists("asdadasafds", "col")) {
+  query("ALTER TABLE asdadasafds ADD col VARCHAR(255) NOT NULL AFTER `dasfdsf`;");
+
+  echo "column col in asdadasafds added! <br>";
 }
