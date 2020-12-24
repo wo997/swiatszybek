9f7ee2f873cd2a2ca3a98f411c92dafbf54a36a3
wo997wo997 <wojtekwo997@gmail.com>
2020-12-24 21:14:27 +0100

    wip

--- entities/pies/test3.php
+++ entities/pies/test3.php
@@ -1,14 +1,22 @@
-<?php
+<?php //route[pies]
 
 // the number 1 will act like cat type, rly silly but stick to it pls
 $a = 1;
 
 $a["age"]["value"] = "1"; // = "hehe";
 
-$a["age"]["value"]; 
+$a["age"]["value"];
 
 /*$b = 2;
 
 if ($a > 0) {
     $b++;
 }*/
+
+$pies = [
+    "sum" => function ($a, $b) {
+        return $a + $b;
+    },
+];
+
+var_dump($pies["sum"](1, 2));
