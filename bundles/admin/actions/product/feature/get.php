b7804c90365c31402b734512dc9168eb81f09fac
wo997wo997 <wojtekwo997@gmail.com>
2021-02-06 20:11:23 +0100

    dfg

---
+++ bundles/admin/actions/product/feature/get.php
@@ -0,0 +1,7 @@
+<?php //route[{ADMIN}product/feature/get]
+
+$id = Request::urlParam(4);
+
+$product_feature = EntityManager::getEntityById("product_feature", $id);
+
+Request::jsonResponse(["product_feature" => $product_feature->getProps()]);
