ebcf116e092d0da6beec0220ea07a6497d2125ae
wo997wo997 <wojtekwo997@gmail.com>
2021-02-16 23:50:09 +0100

    dfg

--- src/js/components/batch_traits/list_controls.js
+++ src/js/components/batch_traits/list_controls.js
@@ -7,14 +7,10 @@
 
 registerCompBatchTrait(
 	"list_controls",
-	IS_TOUCH_DEVICE
-		? html`
-				<p-trait data-trait="list_down_btn"></p-trait>
-				<p-trait data-trait="list_up_btn"></p-trait>
-				<p-trait data-trait="list_delete_btn"></p-trait>
-		  `
-		: html`
-				<p-trait data-trait="list_grab_btn"></p-trait>
-				<p-trait data-trait="list_delete_btn"></p-trait>
-		  `
+	html`
+		<p-trait data-trait="list_grab_btn"></p-trait>
+		<p-trait data-trait="list_down_btn"></p-trait>
+		<p-trait data-trait="list_up_btn"></p-trait>
+		<p-trait data-trait="list_delete_btn"></p-trait>
+	`
 );
