d290c829ed3bd76d019e3a45c2c7311911280733
wo997wo997 <wojtekwo997@gmail.com>
2021-02-16 20:38:29 +0100

    dfg

--- global/footer.php
+++ global/footer.php
@@ -27,59 +27,6 @@
     </div>
 <?php endif ?>
 
-<!-- TODO: inject with js -->
-<div id="loginForm" data-form data-modal data-dismissable class="loginForm">
-    <div class="modal-body">
-        <button class="close-modal-btn"><i class="fas fa-times"></i></button>
-
-        <h3 class="modal-header">
-            <img class="user-icon" src="/src/img/user_icon.svg">
-            Logowanie
-        </h3>
-
-        <div class="scroll-panel scroll-shadow panel-padding">
-            <div>
-                <div class="label first">E-mail</div>
-                <input class="field" type="text" autocomplete="username" name="email" data-validate="backend|custom:validateLoginUserEmail|delay:300">
-
-                <div class="label">
-                    <span>Hasło</span>
-                    <i class="fas fa-eye btn small" onclick="togglePasswordFieldType(this,$(`#loginForm [name='password']`))" data-tooltip="Pokaż hasło" data-tooltip_position="right"></i>
-                </div>
-                <input class="field" type="password" autocomplete="current-password" name="password" data-validate="backend|blank_on_change:true">
-
-                <div style="margin-top:10px;text-align:center">
-                    <label class="checkbox-wrapper">
-                        <input type="checkbox" name="remember_me" value="1">
-                        <div class="checkbox"></div>
-                        Zapamiętaj mnie
-                    </label>
-                </div>
-
-                <button class="btn primary medium login_btn" style="margin:10px 0; width: 100%" onclick="login()" data-submit>
-                    <span>Zaloguj się <i class="fa fa-chevron-right"></i></span>
-                    <i class="fa fa-check"></i>
-                </button>
-
-                <div style="text-align: center; padding: 10px 0">
-                    <a href="/rejestracja" class="btn buff subtle" style="font-weight: 600;">
-                        Zarejestruj się <i class="fa fa-user-plus"></i>
-                    </a>
-                    <div style="height: 10px;"></div>
-                    <a href="/resetowanie-hasla" class="btn buff subtle" style="font-weight: 600;">
-                        Zresetuj hasło <i class="fa fa-lock-open"></i>
-                    </a>
-                </div>
-
-                <div style="text-align: center;">lub</div>
-                <div class="g-signin2" data-onsuccess="onSignIn"></div>
-
-                <?= $fb_login_btn ?>
-            </div>
-        </div>
-    </div>
-</div>
-
 <?php
 
 if (isset($CSS_files)) {
--- src/js/user.js
+++ src/js/user.js
@@ -108,3 +108,72 @@ window.addEventListener("modal-show", (event) => {
 	}
 	hideLoginFormPassword();
 });
+
+domload(() => {
+	registerModalContent(
+		html`<div id="loginForm" data-form data-modal data-dismissable class="loginForm">
+			<div class="modal-body">
+				<button class="close-modal-btn"><i class="fas fa-times"></i></button>
+
+				<h3 class="modal-header">
+					<img class="user-icon" src="/src/img/user_icon.svg" />
+					Logowanie
+				</h3>
+
+				<div class="scroll-panel scroll-shadow panel-padding">
+					<div>
+						<div class="label first">E-mail</div>
+						<input
+							class="field"
+							type="text"
+							autocomplete="username"
+							name="email"
+							data-validate="backend|custom:validateLoginUserEmail|delay:300"
+						/>
+
+						<div class="label">
+							<span>Hasło</span>
+							<i
+								class="fas fa-eye btn small"
+								onclick="togglePasswordFieldType(this,$(this)._parent()._next())"
+								data-tooltip="Pokaż hasło"
+								data-tooltip_position="right"
+							></i>
+						</div>
+						<input
+							class="field"
+							type="password"
+							autocomplete="current-password"
+							name="password"
+							data-validate="backend|blank_on_change:true"
+						/>
+
+						<div style="margin-top:10px;text-align:center">
+							<label class="label">
+								<p-checkbox class="inline square" style="margin-right:4px"></p-checkbox>
+								Zapamiętaj mnie
+							</label>
+						</div>
+
+						<button class="btn primary medium login_btn" style="margin:10px 0; width: 100%" onclick="login()" data-submit>
+							<span>Zaloguj się <i class="fa fa-chevron-right"></i></span>
+							<i class="fa fa-check"></i>
+						</button>
+
+						<div style="text-align: center; padding: 10px 0">
+							<a href="/rejestracja" class="btn buff subtle" style="font-weight: 600;"> Zarejestruj się <i class="fa fa-user-plus"></i> </a>
+							<div style="height: 10px;"></div>
+							<a href="/resetowanie-hasla" class="btn buff subtle" style="font-weight: 600;">
+								Zresetuj hasło <i class="fa fa-lock-open"></i>
+							</a>
+						</div>
+
+						<div style="text-align: center;">lub</div>
+						<div class="g-signin2" data-onsuccess="onSignIn"></div>
+					</div>
+				</div>
+			</div>
+		</div>`
+	);
+	//<?= $fb_login_btn ?>
+});
