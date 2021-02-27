9628b91f87f178cef005ff602cfe7d1076e98d86
wo997wo997 <wojtekwo997@gmail.com>
2021-02-27 15:33:48 +0100

    dfggdf

--- global/header_logged_in.php
+++ global/header_logged_in.php
@@ -13,13 +13,13 @@ else if ($user_type == 'facebook') $display_user = "<i class=\"fab fa-facebook-s
         <div class="username"><?= $display_user ?></div>
         <hr class="indent">
         <?php if (User::getCurrent()->priveleges["backend_access"]) : ?>
-            <a class="admin border-bottom border-top menu_item gray_hover" href="<?= Request::$static_urls["ADMIN"] ?>zamowienia"> <i class="fas fa-user-cog"></i> Panel administratora</a>
+            <a class="menu_item gray_hover semi-bold" style="margin-bottom:7px" href="<?= Request::$static_urls["ADMIN"] ?>zamowienia"> <i class="fas fa-user-cog"></i> Panel administratora</a>
         <?php endif ?>
         <a class="menu_item gray_hover" href="/moje-konto"> <i class="fas fa-box"></i> Moje zamówienia</a>
         <a class="menu_item gray_hover" href="/moje-konto/dane-uzytkownika"> <i class="fas fa-address-book"></i> Dane użytkownika</a>
         <?php if (User::getCurrent() == 'regular') : ?>
             <a class="menu_item gray_hover" href="/moje-konto/resetowanie-hasla"> <i class="fas fa-unlock-alt"></i> Zmiana hasła </a>
         <?php endif ?>
-        <a class="menu_item gray_hover border-top" onclick="return logout()"> <i class="fa fa-sign-out-alt" style="vertical-align: middle;"></i> Wyloguj się </a>
+        <a class="menu_item gray_hover" style="margin-top:7px" onclick="return logout()"> <i class="fa fa-sign-out-alt" style="vertical-align: middle;"></i> Wyloguj się </a>
     </div>
 </div>
