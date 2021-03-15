bd2063a05e2f757ddd83a5a83a8b35e390a5b00e
wo997wo997 <wojtekwo997@gmail.com>
2021-03-15 23:12:44 +0100

    sdfdsf

--- bundles/admin/views/cms/scripts.js
+++ bundles/admin/views/cms/scripts.js
@@ -208,14 +208,15 @@ domload(() => {
 		//const focus_html_node = focus_node ? focus_node._parent("*", { skip: 0 }) : undefined;
 		//const id = focus_html_node ? +focus_html_node.dataset.ped : 0;
 		const id = focus_node ? +focus_node.dataset.ped : 0;
-		const virtual_node = findNodeInVirtualDom(id);
+		const virtual_node_data = findNodeInVirtualDom(id);
+		const v_node = virtual_node_data ? virtual_node_data.node : undefined;
 
 		if (ev.key.length === 1 && sel) {
 			ev.preventDefault();
 
-			const text = virtual_node.node.text;
+			const text = v_node.text;
 			if (typeof text === "string") {
-				virtual_node.node.text = text.substr(0, focusOffset) + ev.key + text.substr(focusOffset);
+				v_node.text = text.substr(0, focusOffset) + ev.key + text.substr(focusOffset);
 				recreateDom();
 
 				const node_ref = piep_editor_content._child(`[data-ped="${id}"]`);
@@ -226,28 +227,30 @@ domload(() => {
 			}
 		}
 
-		if (ev.key === "Backspace" && virtual_node) {
+		if (ev.key === "Backspace" && virtual_node_data) {
 			ev.preventDefault();
 
-			const text = virtual_node.node.text;
+			const text = v_node.text;
 			if (focusOffset <= 0) {
-				const prev_index = virtual_node.index - 1;
+				const prev_index = virtual_node_data.index - 1;
 				if (prev_index >= 0) {
-					const prev_v_node = virtual_node.children[prev_index];
-					const prev_id = prev_v_node.id;
+					const prev_v_node = virtual_node_data.children[prev_index];
 
-					const prev_v_node_text_before = prev_v_node.text;
-					prev_v_node.text = prev_v_node_text_before + virtual_node.node.text;
-					virtual_node.children.splice(virtual_node.index, 1);
-					recreateDom();
+					if (prev_v_node.text !== undefined) {
+						const prev_id = prev_v_node.id;
+						const prev_v_node_text_before = prev_v_node.text;
+						prev_v_node.text = prev_v_node_text_before + v_node.text;
+						virtual_node_data.children.splice(virtual_node_data.index, 1);
+						recreateDom();
 
-					const prev_node_ref = piep_editor_content._child(`[data-ped="${prev_id}"]`);
-					if (prev_node_ref) {
-						selectElementContentsByIndex(prev_node_ref, prev_v_node_text_before.length);
+						const prev_node_ref = piep_editor_content._child(`[data-ped="${prev_id}"]`);
+						if (prev_node_ref) {
+							selectElementContentsByIndex(prev_node_ref, prev_v_node_text_before.length);
+						}
 					}
 				}
 			} else {
-				virtual_node.node.text = text.substr(0, focusOffset - 1) + text.substr(focusOffset);
+				v_node.text = text.substr(0, focusOffset - 1) + text.substr(focusOffset);
 				recreateDom();
 
 				const node_ref = piep_editor_content._child(`[data-ped="${id}"]`);
@@ -257,28 +260,30 @@ domload(() => {
 			}
 		}
 
-		if (ev.key === "Delete" && virtual_node) {
+		if (ev.key === "Delete" && virtual_node_data) {
 			ev.preventDefault();
 
-			const text = virtual_node.node.text;
-			if (focusOffset >= focus_node.textContent.length) {
-				const next_index = virtual_node.index + 1;
-				if (next_index < virtual_node.children.length) {
-					const next_v_node = virtual_node.children[next_index];
-					const prev_id = next_v_node.id;
+			const text = v_node.text;
+			if (focusOffset >= v_node.text.length) {
+				const next_index = virtual_node_data.index + 1;
+				if (next_index < virtual_node_data.children.length) {
+					const next_v_node = virtual_node_data.children[next_index];
 
-					const next_v_node_text_before = next_v_node.text;
-					next_v_node.text = next_v_node_text_before + virtual_node.node.text;
-					virtual_node.children.splice(virtual_node.index, 1);
-					recreateDom();
+					if (next_v_node.text !== undefined) {
+						const node_id = v_node.id;
+						const v_node_text_before = v_node.text;
+						v_node.text = v_node_text_before + next_v_node.text;
+						virtual_node_data.children.splice(next_index, 1);
+						recreateDom();
 
-					const next_node_ref = piep_editor_content._child(`[data-ped="${prev_id}"]`);
-					if (next_node_ref) {
-						selectElementContentsByIndex(next_node_ref, next_v_node_text_before.length);
+						const node_ref = piep_editor_content._child(`[data-ped="${node_id}"]`);
+						if (node_ref) {
+							selectElementContentsByIndex(node_ref, v_node_text_before.length);
+						}
 					}
 				}
 			} else {
-				virtual_node.node.text = text.substr(0, focusOffset) + text.substr(focusOffset + 1);
+				v_node.text = text.substr(0, focusOffset) + text.substr(focusOffset + 1);
 				recreateDom();
 
 				const node_ref = piep_editor_content._child(`[data-ped="${id}"]`);
@@ -303,7 +308,7 @@ domload(() => {
 		if (ev.key === "ArrowRight") {
 			ev.preventDefault();
 
-			if (focusOffset >= focus_node.textContent.length) {
+			if (focusOffset >= v_node.text.length) {
 				const next_textable = getTextable(focus_node, 1);
 				if (next_textable) {
 					selectElementContentsByIndex(next_textable, 0);
@@ -312,17 +317,32 @@ domload(() => {
 				selectElementContentsByIndex(focus_node, focusOffset + 1);
 			}
 		}
-		if (ev.key === "Enter" && virtual_node) {
+
+		if (ev.key === "ArrowDown") {
+			//selectElementContentsFromOther()
 			ev.preventDefault();
 
-			const text = virtual_node.node.text;
+			if (focusOffset >= v_node.text.length) {
+				const next_textable = getTextable(focus_node, 1);
+				if (next_textable) {
+					selectElementContentsByIndex(next_textable, 0);
+				}
+			} else {
+				selectElementContentsByIndex(focus_node, focusOffset + 1);
+			}
+		}
+
+		if (ev.key === "Enter" && virtual_node_data) {
+			ev.preventDefault();
+
+			const text = v_node.text;
 			if (typeof text === "string") {
-				const insert_v_node = cloneObject(virtual_node.node);
+				const insert_v_node = cloneObject(v_node);
 				insert_v_node.text = text.substr(focusOffset);
 				const insert_node_id = getNewId();
 				insert_v_node.id = insert_node_id;
-				virtual_node.node.text = text.substr(0, focusOffset);
-				virtual_node.children.splice(virtual_node.index + 1, 0, insert_v_node);
+				v_node.text = text.substr(0, focusOffset);
+				virtual_node_data.children.splice(virtual_node_data.index + 1, 0, insert_v_node);
 				recreateDom();
 
 				const insert_node_ref = piep_editor_content._child(`[data-ped="${insert_node_id}"]`);
@@ -398,10 +418,9 @@ function updatePiepCursorPosition() {
 /**
  *
  * @param {PiepNode} node
- * @param {PiepNode} from
  * @returns
  */
-function selectElementContentsFromOther(node, from) {
+function selectElementContentsFromOther(node) {
 	const text_node = node.childNodes[0];
 	const sel = window.getSelection();
 	const range = document.createRange();
