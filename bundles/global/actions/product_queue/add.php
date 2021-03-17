ba2dd85f40585f356c5cfc95fb31a310034e1b97
wo997wo997 <wojtekwo997@gmail.com>
2021-03-17 22:13:13 +0100

    sdfsd

--- bundles/global/actions/product_queue/add.php
+++ bundles/global/actions/product_queue/add.php
@@ -1,8 +1,17 @@
 <?php //route[/product_queue/add]
 
 try {
+    $product_queue_data = json_decode($_POST["product_queue"], true);
+
+    $product_queue_data["email"] = trim($product_queue_data["email"]);
+    if (DB::fetchVal("SELECT 1 FROM product_queue WHERE email LIKE ?", [$product_queue_data["email"]])) {
+        Request::jsonResponse(["success" => true]);
+        // alrady added but dont say it lol
+        //Request::jsonResponse(["success" => false, "message" => ""]);
+    }
+
     DB::beginTransaction();
-    $product_queue = EntityManager::getEntity("product_queue", json_decode($_POST["product_queue"], true));
+    $product_queue = EntityManager::getEntity("product_queue", $product_queue_data);
     EntityManager::saveAll();
     DB::commitTransaction();
     Request::jsonResponse(["success" => true]);
