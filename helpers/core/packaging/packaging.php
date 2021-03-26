7ec14ac3964a47113ab2d36c990ab6154ad12e4a
wo997wo997 <wojtekwo997@gmail.com>
2021-03-26 04:24:02 +0100

    I'm a fabulous mathmatician

--- helpers/core/packaging/packaging.php
+++ helpers/core/packaging/packaging.php
@@ -109,74 +109,88 @@ function putBoxIntoPackage3D($package_dims, $products_dims, $contents = [])
     for ($product_index = 0; $product_index < $top; $product_index++) {
         $products_dims_copy = $products_dims;
         $product_to_put_dims = array_splice($products_dims_copy, $product_index, 1)[0];
-        foreach ($orientations as $orientation) {
-            $final_product_to_put_dims = [
-                $product_to_put_dims[$orientation[0]],
-                $product_to_put_dims[$orientation[1]],
-                $product_to_put_dims[$orientation[2]],
+
+        if (!$contents) {
+            $starts = [
+                [0, 0, 0]
             ];
-
-            if (!$contents) {
-                $starts = [
-                    [0, 0, 0]
-                ];
-            } else {
-                $starts = [];
-                foreach ($contents as $two_points_1) {
-                    array_push(
-                        $starts,
-                        [
-                            $two_points_1[1][0],
-                            $two_points_1[0][1],
-                            $two_points_1[0][2],
-                        ],
-                        [
-                            $two_points_1[0][0],
-                            $two_points_1[1][1],
-                            $two_points_1[0][2],
-                        ],
-                        [
-                            $two_points_1[0][0],
-                            $two_points_1[0][1],
-                            $two_points_1[1][2],
-                        ],
-                    );
-                }
+        } else {
+            $starts = [];
+            foreach ($contents as $two_points_1) {
+                array_push(
+                    $starts,
+                    [
+                        $two_points_1[1][0],
+                        $two_points_1[0][1],
+                        $two_points_1[0][2],
+                    ],
+                    [
+                        $two_points_1[0][0],
+                        $two_points_1[1][1],
+                        $two_points_1[0][2],
+                    ],
+                    [
+                        $two_points_1[0][0],
+                        $two_points_1[0][1],
+                        $two_points_1[1][2],
+                    ],
+                );
             }
+        }
 
-            foreach ($starts as $start) {
+        $outside_for_all_orientations_and_positions = true;
+
+        $two_points_to_puts = [];
+        foreach ($starts as $start) {
+            foreach ($orientations as $orientation) {
+                $final_product_to_put_dims = [
+                    $product_to_put_dims[$orientation[0]],
+                    $product_to_put_dims[$orientation[1]],
+                    $product_to_put_dims[$orientation[2]],
+                ];
                 $end = [
                     $start[0] + $final_product_to_put_dims[0],
                     $start[1] + $final_product_to_put_dims[1],
                     $start[2] + $final_product_to_put_dims[2],
                 ];
+
                 //var_dump(["start", $start, "end", $end]);
-                if ($end[0] > $package_dims[0] || $end[1] > $package_dims[1] || $end[2] > $package_dims[2]) {
-                    continue;
+                if ($end[0] <= $package_dims[0] && $end[1] <= $package_dims[1] && $end[2] <= $package_dims[2]) {
+                    // inside the box, keep going ;)
+                    $outside_for_all_orientations_and_positions = false;
+                    $two_points_to_puts[] = [$start, $end];
                 }
+            }
+        }
 
-                foreach ($contents as $two_points_2) {
-                    if ($start[0] < $two_points_2[1][0] && $start[1] < $two_points_2[1][1] && $start[2] < $two_points_2[1][2]) {
-                        break 2;
-                    }
+        if ($outside_for_all_orientations_and_positions) {
+            return false;
+        }
+
+        foreach ($two_points_to_puts as $two_points_to_put) {
+            $start =  $two_points_to_put[0];
+            $end =  $two_points_to_put[1];
+
+            foreach ($contents as $two_points_2) {
+                if ($start[0] < $two_points_2[1][0] && $start[1] < $two_points_2[1][1] && $start[2] < $two_points_2[1][2]) {
+                    // kinda behind other points
+                    break 2;
                 }
+            }
 
-                $contents_copy = [
-                    [$start, $end]
-                ];
-                //var_dump(["AAA", $contents_copy]);
+            $contents_copy = [
+                [$start, $end]
+            ];
 
-
-                foreach ($contents as $two_points_2) {
-                    if ($two_points_2[1][0] >= $end[0] || $two_points_2[1][1] >= $end[1] || $two_points_2[1][2] >= $end[2]) {
-                        //var_dump("XXX", $two_points_2);
-                        $contents_copy[] = $two_points_2;
-                    }
+            foreach ($contents as $two_points_2) {
+                if ($two_points_2[1][0] >= $end[0] || $two_points_2[1][1] >= $end[1] || $two_points_2[1][2] >= $end[2]) {
+                    //var_dump("XXX", $two_points_2);
+                    $contents_copy[] = $two_points_2;
                 }
+            }
 
-                if (putBoxIntoPackage3D($package_dims, $products_dims_copy, $contents_copy)) {
-                    return true;
-                }
+            if (putBoxIntoPackage3D($package_dims, $products_dims_copy, $contents_copy)) {
+                return true;
             }
         }
     }
