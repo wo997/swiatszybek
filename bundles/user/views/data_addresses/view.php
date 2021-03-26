159a1fe27e0e1d65db5346f6fc775b92063c4a3e
wo997wo997 <wojtekwo997@gmail.com>
2021-03-26 22:45:37 +0100

    sdfsdf

---
+++ bundles/user/views/data_addresses/view.php
@@ -0,0 +1,12 @@
+<?php //route[{USER}/dane-adresy]
+
+?>
+
+<?php startSection("head_content"); ?>
+
+<title>Dane / Adresy</title>
+
+<?php startSection("body_content"); ?>
+
+
+<?php include "bundles/user/templates/default.php"; ?>
