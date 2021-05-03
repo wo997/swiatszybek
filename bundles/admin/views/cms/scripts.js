4c597994fb4948298228b57d95a61c4d62e2b6a7
wo997wo997 <wojtekwo997@gmail.com>
2021-05-03 18:50:29 +0200

    sdfsdf

--- bundles/admin/views/cms/scripts.js
+++ bundles/admin/views/cms/scripts.js
@@ -98,251 +98,47 @@ domload(() => {
 		previewUrl(preview_url);
 	});
 
+	let v_dom_json;
+
 	if (page_data) {
-		try {
-			const v_dom = JSON.parse(page_data.v_dom_json);
-			piep_cms.import(v_dom);
-		} catch {}
+		v_dom_json = page_data.v_dom_json;
 	}
 
 	if (template_data) {
-		try {
-			const v_dom = JSON.parse(template_data.v_dom_json);
-			piep_cms.import(v_dom);
-		} catch {}
+		v_dom_json = template_data.v_dom_json;
 	}
 
-	return;
-	piep_cms.import([
-		{
-			id: 100,
-			tag: "h1",
-			text: "Dobry frejmwork",
-			styles: { fontSize: "1.4rem", fontWeight: "var(--bold)", marginTop: "50px" },
-			attrs: {},
-			classes: [],
-		},
-		{
-			id: 101,
-			tag: "p",
-			text:
-				"Wirtualny DOM krul. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			styles: {
-				marginTop: "20px",
-				marginLeft: "-10px",
-				marginBottom: "-50px",
-				marginRight: "30px",
-				paddingTop: "20px",
-				paddingLeft: "30px",
-				paddingBottom: "40px",
-				paddingRight: "50px",
-				backgroundColor: "var(--clr_primary)",
-			},
-			attrs: {},
-			classes: [],
-		},
-		{
-			id: 102,
-			tag: "h1",
-			text: "123234234563456",
-			styles: { fontSize: "1.4rem", fontWeight: "var(--normal)", color: "#d5d" },
-			attrs: {},
-			classes: [],
-		},
-		{
-			id: 103,
-			tag: "p",
-			text:
-				"Wirtualny DOM krul. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			styles: { marginTop: "20px" },
-			attrs: {},
-			classes: [],
-		},
-		{
-			tag: "div",
-			id: 1234,
-			text: undefined,
-			styles: { padding: "10px", border: "1px solid yellow" },
-			classes: ["columns_container"],
-			attrs: {},
-			children: [
-				{
-					id: 1238,
-					tag: "div",
-					styles: { padding: "10px", border: "1px solid red" },
-					attrs: {},
-					classes: ["vertical_container"],
-					children: [
-						{
-							id: 1235,
-							tag: "p",
-							text: "AAA<br>AAA<br>AAA<br>AAA<br>AAA<br>AAA<br>AAA<br>AAA<br>AAA<br>AAA<br>",
-							styles: { padding: "10px", border: "1px solid pink" },
-							attrs: {},
-							classes: [],
-						},
-					],
-				},
-				{
-					id: 1239,
-					tag: "div",
-					styles: { padding: "10px", border: "1px solid red" },
-					attrs: {},
-					classes: ["vertical_container"],
-					children: [],
-				},
-				{
-					id: 1241,
-					tag: "div",
-					styles: { padding: "10px", border: "1px solid red" },
-					attrs: {},
-					classes: ["vertical_container"],
-					children: [
-						{
-							id: 1233,
-							tag: "p",
-							text: "BBB",
-							styles: { padding: "10px", border: "1px solid pink" },
-							attrs: {},
-							classes: [],
-						},
-						{
-							id: 1133,
-							tag: "p",
-							text: "CCC",
-							styles: { padding: "10px", border: "1px solid blue" },
-							attrs: {},
-							classes: [],
-						},
-					],
-				},
-			],
-		},
-		{
-			id: 104,
-			tag: "h1",
-			text: "Dobry frejmwork aaaaaaaaaa",
-			styles: { fontSize: "1.4rem", fontWeight: "var(--semi_bold)", color: "#d5d" },
-			attrs: {},
-			classes: [],
-		},
-		{
-			id: 105,
-			tag: "p",
-			text:
-				"Wirtualny DOM krul. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			styles: { marginTop: "20px" },
-			attrs: {},
-			classes: [],
-		},
-		// {
-		// 	id: 169,
-		// 	tag: "div",
-		// 	styles: { display: "flex", borderWidth: "1px", borderColor: "#00c", borderStyle: "solid", padding: "10px" },
-		// 	attrs: {},
-		// 	classes: [],
-		// 	children: [
-		// 		{
-		// 			id: 170,
-		// 			tag: "div",
-		// 			styles: {
-		// 				display: "flex",
-		// 				borderWidth: "1px",
-		// 				borderColor: "#00c",
-		// 				borderStyle: "solid",
-		// 				padding: "10px",
-		// 				flexDirection: "column",
-		// 			},
-		// 			attrs: {},
-		// 			classes: [],
-		// 			children: [
-		// 				{
-		// 					id: 171,
-		// 					tag: "p",
-		// 					text: "dfgsdgs sdgf sdfgsdg fsdgfsdgfsdfgsd fgsdg f",
-		// 					styles: { display: "flex", borderWidth: "1px", borderColor: "#c00", borderStyle: "solid", padding: "10px" },
-		// 					attrs: {},
-		// 					classes: [],
-		// 				},
-		// 				{
-		// 					id: 172,
-		// 					tag: "p",
-		// 					text: "ADFG DFGSDG SDFGSDF GSDFG SDFG SDGFD FGSDGFS",
-		// 					styles: { display: "flex", borderWidth: "1px", borderColor: "#0c0", borderStyle: "solid", padding: "10px" },
-		// 					attrs: {},
-		// 					classes: [],
-		// 				},
-		// 			],
-		// 		},
-		// 		{
-		// 			id: 173,
-		// 			tag: "p",
-		// 			text: "dfgsdgs sdgf sdfgsdg fsdgfsdgfsdfgsd fgsdg f",
-		// 			styles: { display: "flex", borderWidth: "1px", borderColor: "#c00", borderStyle: "solid", padding: "10px" },
-		// 			attrs: {},
-		// 			classes: [],
-		// 		},
-		// 		{
-		// 			id: 174,
-		// 			tag: "p",
-		// 			text: "ADFG DFGSDG SDFGSDF GSDFG SDFG SDGFD FGSDGFS",
-		// 			styles: { display: "flex", borderWidth: "1px", borderColor: "#0c0", borderStyle: "solid", padding: "10px" },
-		// 			attrs: {},
-		// 			classes: [],
-		// 		},
-		// 	],
-		// },
-		{
-			id: 10,
-			tag: "img",
-			styles: { width: "50%" },
-			attrs: {
-				"data-src": "/uploads/-/2021-04-02-19-41-1_559x377.jpg",
-			},
-			classes: ["wo997_img"],
-		},
-		{
-			id: 107,
-			tag: "p",
-			text:
-				"Wirtualny DOM krul. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-			styles: { marginTop: "20px", fontSize: "30px" },
-			attrs: {},
-			classes: [],
-		},
-		{
-			id: 3,
-			tag: "div",
+	/** @type {vDomNode[]} */
+	let v_dom = [];
+	try {
+		v_dom = JSON.parse(v_dom_json);
+	} catch {}
+	let full_v_dom = v_dom;
 
-			styles: {},
-			children: [
-				{ id: 4, tag: "p", text: "dziecko 1", styles: {}, attrs: {}, classes: [] },
-				{ id: 8, tag: "p", text: "", styles: {}, attrs: {}, classes: [] },
-				{
-					id: 5,
-					tag: "p",
-					children: [
-						{ id: 6, tag: "span", text: "dziecko 2.1", styles: {}, attrs: {}, classes: [] },
-						{ id: 7, tag: "span", text: "dziecko 2.2", styles: {}, attrs: {}, classes: [] },
-					],
-					styles: {},
-					attrs: {},
-					classes: [],
-				},
-			],
-			attrs: {},
-			classes: [],
-		},
-		{
-			id: 9,
-			tag: "div",
-			styles: {
-				backgroundColor: "red",
-				padding: "20px",
-			},
-			children: [],
-			attrs: {},
-			classes: [],
-		},
-	]);
+	parent_templates.forEach((parent_template) => {
+		/** @type {vDomNode[]} */
+		let parent_template_v_dom = [];
+		try {
+			parent_template_v_dom = JSON.parse(parent_template.v_dom_json);
+		} catch {}
+
+		/**
+		 * @param {vDomNode[]} v_nodes
+		 */
+		const traverseVDom = (v_nodes) => {
+			for (const v_node of v_nodes) {
+				if (v_node.module_name === "template_hook" && v_node.settings && v_node.settings.template_hook_name) {
+					console.log(v_node.settings.template_hook_name);
+				}
+				if (v_node.children) {
+					traverseVDom(v_node.children);
+				}
+			}
+		};
+		traverseVDom(parent_template_v_dom);
+
+		full_v_dom = parent_template_v_dom;
+	});
+
+	piep_cms.import(full_v_dom);
 });
--- bundles/admin/views/cms/vars.js
+++ bundles/admin/views/cms/vars.js
@@ -1,2 +1,3 @@
 let page_data;
 let template_data;
+let parent_templates;
