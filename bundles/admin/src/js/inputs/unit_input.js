22edb9c4c1703cdcc1a0bc4fcf00a1aef48acff4
wo997wo997 <wojtekwo997@gmail.com>
2021-05-28 18:26:42 +0200

    asda

--- bundles/admin/src/js/inputs/unit_input.js
+++ bundles/admin/src/js/inputs/unit_input.js
@@ -20,12 +20,18 @@ function registerUnitInputs(parent) {
 		input.classList.add("field");
 		select.classList.add("field", "inline", "blank", "unit_picker");
 
+		let setting_value = false;
+
 		/**
 		 *
 		 * @param {string} value
 		 * @param {SetDataOptions} options
 		 */
 		container._set_value = (value = null, options = {}) => {
+			if (setting_value) {
+				return;
+			}
+
 			value = def(value, "");
 			let unit;
 			for (const option of select.options) {
@@ -48,10 +54,22 @@ function registerUnitInputs(parent) {
 			input._set_value(value.substr(0, value.length - unit.length), { quiet: true });
 		};
 
+		let last_val = 0;
 		const anyChange = () => {
+			const new_val = container._get_value();
+			if (new_val === last_val) {
+				return;
+			}
+			last_val = new_val;
+
+			setting_value = true;
 			container._dispatch_change();
+			setTimeout(() => {
+				setting_value = false;
+			});
 		};
 
+		input.addEventListener("value_set", anyChange);
 		input.addEventListener("change", anyChange);
 		input.addEventListener("input", anyChange);
 		select.addEventListener("change", anyChange);
