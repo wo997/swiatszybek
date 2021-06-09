2bc69523d16dc9649648d576e899617bee17d82c
wo997wo997 <wojtekwo997@gmail.com>
2021-06-09 05:09:22 +0200

    sdsad

---
+++ bundles/admin/views/cms/blcs/slider/schema.php
@@ -0,0 +1,22 @@
+<?php //hook[register]
+
+PiepCMSManager::registerModule([
+    "name" => "slider",
+    "render" => function ($params) {
+        $children = def($params, ["v_node", "children"]);
+        $slides_html = "";
+        if ($children) {
+            $slides_data = traverseVDom($children, ["html_only" => true]);
+            $slides_html = $slides_data["content_html"];
+        }
+        ob_start();
+?>
+    <div class="wo997_slider">
+        <div class="wo997_slides_container">
+            <div class="wo997_slides_wrapper"><?= $slides_html ?></div>
+        </div>
+    </div>
+<?php
+        return ob_get_clean();
+    },
+]);
