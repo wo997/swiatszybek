00ef7727e89ec9af9154241b93776cad94e1ebd7
wo997wo997 <wojtekwo997@gmail.com>
2021-06-14 04:50:37 +0200

    sdsfd

--- bundles/admin/actions/gus/regon.php
+++ bundles/admin/actions/gus/regon.php
@@ -1,52 +1,20 @@
 <?php //route[{ADMIN}/gus/regon]
 
-// function do_something($params)
-// {
-//     global $gg_token;
 
-//     $curl = curl_init();
+use GusApi\GusApi;
+use GusApi\ReportTypes;
+//use GusApi\BulkReportTypes;
 
-//     curl_setopt_array($curl, array(
-//         CURLOPT_VERBOSE => true,
-//         CURLOPT_URL => "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/UslugaBIRzewnPubl.svc/Zaloguj",
+//$gus = new GusApi('your api key here');
+//for development server use:
+$gus = new GusApi('abcde12345abcde12345', 'dev');
 
-//         -H "Content-Type: application/soap+xml;charset=UTF-8"
-//         CURLOPT_POST => 1,
-//         // CURLOPT_HTTPHEADER => array(
-//         //     "Token: $gg_token"
-//         // ),
-//         CURLOPT_POSTFIELDS => http_build_query($params),
-//         CURLOPT_RETURNTRANSFER => true,
-//     ));
+try {
+    $gus->login();
+    $gusReports = $gus->getByNip("5792092290");
 
-//     $response = curl_exec($curl);
-//     //$curl_error = curl_error($curl);
-//     //var_dump("error", $curl_error);
-//     $data = json_decode($response, true);
-
-//     return $data;
-// }
-// $gg_token = do_something(["pKluczUzytkownika" => "abcde12345abcde12345"]);
-
-// var_dump($gg_token);
-
-$stream_context_opts = array(
-    'https' => array(
-        'method' => "POST",
-        'header' => "Content-Type: application/xop+xml; charset=utf-8\r\n"
-    )
-);
-
-$soap_stream_context = stream_context_create($stream_context_opts);
-
-$client = new SoapClient(
-    "https://wyszukiwarkaregontest.stat.gov.pl/wsBIR/wsdl/UslugaBIRzewnPubl-ver11-test.wsdl",
-    [
-        // 'soap_version' => SOAP_1_2,
-        'trace' => 1,
-        'stream_context' => $soap_stream_context,
-    ]
-);
-$client->__soapCall("Zaloguj", ["pKluczUzytkownika" => "abcde12345abcde12345"]);
-
-//var_dump($client);
+    foreach ($gusReports as $gusReport) {
+        echo $gusReport->getName() . " " . $gusReport->getStreet() . ' ' . $gusReport->getPropertyNumber() . '/' . $gusReport->getApartmentNumber();
+    }
+} catch (Exception $e) {
+}
