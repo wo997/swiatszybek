f7dd553ee5717e8f54c2b5533eb3c5d4a74528cc
wo997wo997 <wojtekwo997@gmail.com>
2021-07-01 23:19:25 +0200

    sasd

--- bundles/admin/actions/general_product/save.php
+++ bundles/admin/actions/general_product/save.php
@@ -6,7 +6,8 @@ try {
     $general_product_data = json_decode($_POST["general_product"], true);
 
     $general_product_id = $general_product_data[EntityManager::getEntityIdColumn("general_product")];
-    if ($general_product_id !== -1) {
+    $existed = $general_product_id !== -1;
+    if ($existed) {
         $page_id = DB::fetchVal("SELECT page_id FROM page WHERE link_what_id=$general_product_id AND page_type='general_product'");
         $page = $page_id ? EntityManager::getEntityById("page", $page_id) : null;
         if ($page) {
@@ -15,8 +16,9 @@ try {
     }
 
     $general_product = EntityManager::getEntity("general_product", $general_product_data);
+    $general_product_id = $general_product->getId();
 
-    if ($general_product_id !== -1) {
+    if ($existed) {
         foreach (json_decode(def($_POST, "product_feature_options", "[]"), true) as $product_feature_option_data) {
             $product_feature_option = EntityManager::getEntityById("product_feature_option", $product_feature_option_data["product_feature_option_id"]);
             if (!$product_feature_option) {
@@ -35,8 +37,6 @@ try {
         if (!$page) {
             $general_product->setProp("active", 0);
         }
-    } else {
-        $general_product_id = $general_product->getId();
     }
 
     EntityManager::saveAll();
